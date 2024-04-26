ededbd9274e1f47d1a4ee6226bc2137408ae7869
vitor1234567789 <120030239+vitor1234567789@users.noreply.github.com>
2024-04-26 11:27:41 -0300

    Add files via upload

---
+++ PRO_1-1_C117_TemplateDoProjeto-main/static/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Busque a data atual e atualize-a no DOM
+
+
+
+
+    // Escreva um evento, quando o botão Enviar for clicado
+    $('').click(function(){
+
+        //  Obtenha o valor do texto da área de texto usando o método 'val()'
+        let text_value = $('').val()
+
+        //  Converta-o em um objeto JS.
+        //  Forneça uma "chave" aqui e escreva o mesmo no arquivo app.py também para extrair dados
+        let input_text = {'' : text_value}
+        console.log(input_text)
+
+        //  requisição ajax
+        $.ajax({
+
+            //  tipo da requisição web
+            type : '',
+
+            //  dados a serem enviados no formato JSON
+            data : JSON.stringify(),
+
+            //  o tipo de resposta esperado é json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  se tudo funcionar, execute esta função
+            success : function(result){
+
+                // extraia previsão e a URL do emoticon do resultado
+
+
+                //  atualize os elementos DOM
+
+
+                //  exiba-os
+
+            },
+
+            //  se houver algum erro, execute esta função
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  limpando a caixa de texto após cada pressionamento de botão
+        $('#text').val("")
+    })
+        
+})
